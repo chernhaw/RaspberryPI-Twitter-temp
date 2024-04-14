0d89a98ba8f126fba1b962b9c4b8ffea1e66dbef
chernhaw <38425578+chernhaw@users.noreply.github.com>
2024-04-14 16:48:33 +0800

    added entry for last 7 day temp

--- temp-firebase.py
+++ temp-firebase.py
@@ -2,6 +2,7 @@ import json
 import os
 import time as time
 import pyrebase
+from collections import OrderedDict, deque
 
 from pyrebase import pyrebase
 
@@ -28,6 +29,34 @@ def update_firebase():
         
         data = {"date":localtime, "temp":temperature, "humidity":humidity}
         db.child('sensor/dht').push(data)
+        db.child('sensor/current').update(data)
+        datastr=str(localtime)+"T"+temperature+"H"+humidity
+        update_recent(datastr)
 
+def update_recent(data):
+        
+        
+        fixed_length=6048+168 # 1 week data : 24x4 =168
+        response =""
+        recent_data=""
+        try:
+            response=db.child('sensor/recent').get()
+            
+            if len(response.val())==0:
+                recent_data=data
+            else:
+                recent_data=response.val()+"|"+data
+            
+        except:
+            response=""
+        
+ 
+        
+        if len(recent_data) > fixed_length:
+            recent_data=recent_data[37:]
+        
+        db.child('sensor/recent').set(recent_data)
+          
+        
 update_firebase()
  
